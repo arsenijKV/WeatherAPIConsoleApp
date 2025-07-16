67c50ab243fa21947f77bcd2bf6dfb1e4914633c
arsenijKV <163118921+arsenijKV@users.noreply.github.com>
2025-07-16 14:01:52 +0300

    While 30 minuts

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,20 @@ var builder = Host.CreateDefaultBuilder(args)
 using var host = builder.Build();
 var svc = host.Services.GetRequiredService<IWeatherService>();
 
-var weather = await svc.GetCurrentWeatherAsync("Vilnius");
+while (true)
+{
+    await DoTimer();
+    await Task.Delay(TimeSpan.FromMinutes(30));
+}
+async Task DoTimer()
 
-Console.WriteLine($"{weather.Location.Name}, {weather.Location.Country}");
-Console.WriteLine($" Temperature: {weather.Current.Temp_c}°C");
-Console.WriteLine($" Wind: {weather.Current.Wind_kph} km/h");
-Console.WriteLine($" Humidity: {weather.Current.Humidity}%");
-Console.WriteLine($" Condition: {weather.Current.Condition.Text}");
+{
+    var weather = await svc.GetCurrentWeatherAsync("Vilnius");
+    Console.WriteLine($"{weather.Location.Name}, {weather.Location.Country}");
+    Console.WriteLine($" Temperature: {weather.Current.Temp_c}°C");
+    Console.WriteLine($" Wind: {weather.Current.Wind_kph} km/h");
+    Console.WriteLine($" Humidity: {weather.Current.Humidity}%");
+    Console.WriteLine($" Condition: {weather.Current.Condition.Text}");
+    await Task.CompletedTask;
+
+}
